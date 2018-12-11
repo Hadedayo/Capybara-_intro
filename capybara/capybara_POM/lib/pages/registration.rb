e709c61767f7f775751ad7c74adf8633f315474c
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-12-11 15:32:44 +0000

    Created methods to get elements on the page and writing tests to verify the validity

--- capybara/capybara_POM/lib/pages/registration.rb
+++ capybara/capybara_POM/lib/pages/registration.rb
@@ -9,8 +9,15 @@ class Registration
   LAST_NAME_FIELD_ID = 'lastName'
   AGE_FIELD_BY_TYPE = 'input[type="number"]'
   DOB_FIELD = 'dob'
-  FEMALE_FIELD = 'label[for="customRadioInline2"]'
-
+  FEMALE_FIELD_LABEL = 'label[for="customRadioInline2"]'
+  FEMALE_FIELD_INPUT = 'input[id="customRadioInline2"]'
+  MALE_FIELD_LABEL = 'label[for="customRadioInline1"]'
+  MALE_FIELD_INPUT = 'input[id="customRadioInline1"]'
+  DEGREE_FIELD = 'input[placeholder="Enter Degree"]'
+  UNIVERSITY_FIELD = 'inputUni'
+  ADDRESS = 'input[placeholder="1234 Main St"]'
+  ADDRESS_NO_TWO = 'input[placeholder="Apartment, studio, or floor"]'
+  CITY_FIELD = 'inputCity'
 
   def visit_home_page
     visit(HOMEPAGE_URL)
@@ -37,19 +44,71 @@ class Registration
   end
 
   def check_age_field
-    find(AGE_FIELD_BY_TYPE).value
+    find(AGE_FIELD_BY_TYPE).value.to_i
   end
 
   def fill_in_dob
-    fill_in(DOB_FIELD, :with => '11/12/2018')
+    fill_in(DOB_FIELD, :with => '14/04/1995')
   end
 
   def check_dob_field
     find_field(DOB_FIELD).value
   end
 
-  def check_female_field
-    select(FEMALE_FIELD)
+  def select_female_field
+    find(FEMALE_FIELD_LABEL).click
+  end
+
+  def is_female_selected
+    find(FEMALE_FIELD_INPUT).selected?
+  end
+
+  def select_male_field
+    find(MALE_FIELD_LABEL).click
+  end
+
+  def is_male_selected
+    find(MALE_FIELD_INPUT).selected?
+  end
+
+  def fill_in_degree
+    find(DEGREE_FIELD).send_keys('Chemistry')
+  end
+
+  def check_degree_field
+    find(DEGREE_FIELD).value
+  end
+
+  def select_university
+    select('University of Oxford', :from => UNIVERSITY_FIELD)
+  end
+
+  def is_university_field_selected
+    find_field(UNIVERSITY_FIELD).value
+  end
+
+  def fill_in_address
+    find(ADDRESS).send_keys('23 Bank Street')
+  end
+
+  def check_address_field
+    find(ADDRESS).value
+  end
+
+  def fill_in_address_no_two
+    find(ADDRESS_NO_TWO).send_keys('Flat 3')
+  end
+
+  def check_address_no_two_field
+    find(ADDRESS_NO_TWO).value
+  end
+
+  def fill_in_city
+    find(CITY_FIELD).send_keys('London')
+  end
+
+  def check_city_field
+    find(CITY_FIELD).value
   end
 
 end
--- capybara/capybara_POM/spec/sparta_registration_spec.rb
+++ capybara/capybara_POM/spec/sparta_registration_spec.rb
@@ -5,8 +5,43 @@ describe 'filling in the registration form' do
     it "should show confirmation message once the the form is filled in correctly" do
       @sparta_site = SpartaSite.new
       @sparta_site.sparta_registration.visit_home_page
+
       @sparta_site.sparta_registration.fill_in_first_name_field('Hassanat')
+      expect(@sparta_site.sparta_registration.check_first_name_field).to eq 'Hassanat'
+
+      @sparta_site.sparta_registration.fill_in_last_name_field('Ade')
+      expect(@sparta_site.sparta_registration.check_last_name_field).to eq 'Ade'
+
+      @sparta_site.sparta_registration.fill_in_age_field
+      expect(@sparta_site.sparta_registration.check_age_field).to eq 21
+
+      @sparta_site.sparta_registration.fill_in_dob
+      expect(@sparta_site.sparta_registration.check_dob_field).to eq '1995-04-14'
+
+      @sparta_site.sparta_registration.select_female_field
+      expect(@sparta_site.sparta_registration.is_female_selected).to be true
+      expect(@sparta_site.sparta_registration.is_male_selected).to be false
+
+      @sparta_site.sparta_registration.fill_in_degree
+      expect(@sparta_site.sparta_registration.check_degree_field).to eq 'Chemistry'
+
+      @sparta_site.sparta_registration.select_university
+      expect(@sparta_site.sparta_registration.is_university_field_selected).to eq 'University of Oxford'
+
+      @sparta_site.sparta_registration.fill_in_address
+      expect(@sparta_site.sparta_registration.check_address_field).to eq '23 Bank Street'
+
+      @sparta_site.sparta_registration.fill_in_address_no_two
+      expect(@sparta_site.sparta_registration.check_address_no_two_field).to eq 'Flat 3'
+
+      @sparta_site.sparta_registration.fill_in_city
+      expect(@sparta_site.sparta_registration.check_city_field).to eq 'London'
+      sleep 3
+
+
     end
+
+
 
   end
 end
